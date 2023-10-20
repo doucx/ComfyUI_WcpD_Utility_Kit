e1040d99447391fc561276f6ec83a7add830788c
doucx <doucxldh@gmail.com>
2023-10-21 01:20:01 +0800

    update_init

--- __init__.py
+++ __init__.py
@@ -19,18 +19,18 @@ javascript_folder = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), "js")
 
 if not JS_DEV_MODE and not os.path.exists(extentions_folder):
-    print('Making the "web\extensions\WcpD_Kit" folder')
+    print('- Making the "web\extensions\WcpD_Kit" folder')
     os.mkdir(extentions_folder)
 
 result = filecmp.dircmp(javascript_folder, extentions_folder)
 
 if not JS_DEV_MODE and (result.left_only or result.diff_files):
-    print('Update to javascripts files detected')
+    print('- Update to javascripts files detected')
     file_list = list(result.left_only)
     file_list.extend(x for x in result.diff_files if x not in file_list)
 
     for file in file_list:
-        print(f'Copying {file} to extensions folder')
+        print(f'- Copying {file} to extensions folder')
         src_file = os.path.join(javascript_folder, file)
         dst_file = os.path.join(extentions_folder, file)
         if os.path.exists(dst_file):
