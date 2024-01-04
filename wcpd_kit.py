a914e409106b7b01934ab1aa79718fb4a36e771c
AyeLTesf <doucx@WcpD.wcpd>
2024-01-04 20:13:27 +0800

    delete a node

--- wcpd_kit.py
+++ wcpd_kit.py
@@ -118,45 +118,45 @@ class RandnLatentImage:
         latent = torch.randn([batch_size, 4, height // 8, width // 8])
         return ({"samples":latent}, )
 
-class KSamplerAdvancedWithDenoise:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {"required":
-                    {"model": ("MODEL",),
-                    "add_noise": (["enable", "disable"], ),
-                    "noise_seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
-                    "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
-                    "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step":0.5, "round": 0.01}),
-                    "sampler_name": (comfy.samplers.KSampler.SAMPLERS, ),
-                    "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
-                    "positive": ("CONDITIONING", ),
-                    "negative": ("CONDITIONING", ),
-                    "latent_image": ("LATENT", ),
-                    "start_at_step": ("INT", {"default": 0, "min": 0, "max": 10000}),
-                    "end_at_step": ("INT", {"default": 10000, "min": 0, "max": 10000}),
-                    "return_with_leftover_noise": (["disable", "enable"], ),
-                    "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
-                     }
-                }
+# class KSamplerAdvancedWithDenoise:
+#     @classmethod
+#     def INPUT_TYPES(s):
+#         return {"required":
+#                     {"model": ("MODEL",),
+#                     "add_noise": (["enable", "disable"], ),
+#                     "noise_seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff}),
+#                     "steps": ("INT", {"default": 20, "min": 1, "max": 10000}),
+#                     "cfg": ("FLOAT", {"default": 8.0, "min": 0.0, "max": 100.0, "step":0.5, "round": 0.01}),
+#                     "sampler_name": (comfy.samplers.KSampler.SAMPLERS, ),
+#                     "scheduler": (comfy.samplers.KSampler.SCHEDULERS, ),
+#                     "positive": ("CONDITIONING", ),
+#                     "negative": ("CONDITIONING", ),
+#                     "latent_image": ("LATENT", ),
+#                     "start_at_step": ("INT", {"default": 0, "min": 0, "max": 10000}),
+#                     "end_at_step": ("INT", {"default": 10000, "min": 0, "max": 10000}),
+#                     "return_with_leftover_noise": (["disable", "enable"], ),
+#                     "denoise": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),
+#                      }
+#                 }
 
-    RETURN_TYPES = ("LATENT",)
-    FUNCTION = "sample"
+#     RETURN_TYPES = ("LATENT",)
+#     FUNCTION = "sample"
 
-    CATEGORY = BASE_CATEGORY + "sampler"
+#     CATEGORY = BASE_CATEGORY + "sampler"
 
-    def sample(self, model, add_noise, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, start_at_step, end_at_step, return_with_leftover_noise, denoise):
-        force_full_denoise = True
-        if return_with_leftover_noise == "enable":
-            force_full_denoise = False
-        disable_noise = False
-        if add_noise == "disable":
-            disable_noise = True
-        return common_ksampler(model, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise, disable_noise=disable_noise, start_step=start_at_step, last_step=end_at_step, force_full_denoise=force_full_denoise)
+#     def sample(self, model, add_noise, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, start_at_step, end_at_step, return_with_leftover_noise, denoise):
+#         force_full_denoise = True
+#         if return_with_leftover_noise == "enable":
+#             force_full_denoise = False
+#         disable_noise = False
+#         if add_noise == "disable":
+#             disable_noise = True
+#         return common_ksampler(model, noise_seed, steps, cfg, sampler_name, scheduler, positive, negative, latent_image, denoise=denoise, disable_noise=disable_noise, start_step=start_at_step, last_step=end_at_step, force_full_denoise=force_full_denoise)
 
 NODE_CLASS_MAPPINGS = {
     "MergeStrings": MergeStrings,
     "ExecStrAsCode": ExecStrAsCode,
     "RandnLatentImage": RandnLatentImage,
-    "KSamplerAdvancedWithDenoise": KSamplerAdvancedWithDenoise,
+    # "KSamplerAdvancedWithDenoise": KSamplerAdvancedWithDenoise,
     # "StrTuple": StrTuple
 }
