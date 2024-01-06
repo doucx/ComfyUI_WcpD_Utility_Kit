4290294f4a69a357dd1e08ac103e4f8c3aa4955d
doucx <doucxldh@gmail.com>
2024-01-07 02:17:40 +0800

    add CopyImage

--- wcpd_kit.py
+++ wcpd_kit.py
@@ -1,16 +1,22 @@
 import torch
+import numpy as np
 import os
+import io
 import sys
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
-import comfy
-from nodes import common_ksampler
 import yaml
-# import torch.nn.functional as F
-# import cv2
-# import numpy as np
-# import multiprocessing as mp
-# import time
-# import random
+import subprocess
+import json
+import random
+import pyperclip
+from PIL.PngImagePlugin import PngInfo
+from PIL import Image, ImageOps, ImageSequence
+import tempfile
+
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), "comfy"))
+
+import folder_paths
+import comfy
+from nodes import common_ksampler, SaveImage
 
 BASE_CATEGORY = "WcpD_Kit"
 MAX_RESOLUTION=8192
@@ -89,15 +95,12 @@ class YamlToPrompt:
         return {
             "required": {
                 "text": ("STRING", {
-                    "default":
-                        '''# YamlToPrompt
-                        ''',
                 "multiline": True,
                 }),
             },
         }
 
-    RETURN_TYPES = ("TUPLE",)
+    RETURN_TYPES = ("STRING",)
     FUNCTION = "to_prompt"
     CATEGORY = BASE_CATEGORY + "/text"
 
@@ -114,13 +117,83 @@ class YamlToPrompt:
         return result
 
     def to_prompt(self, text:str):
-        text = yaml.load(text, yaml.FullLoader)
-        text = ", ".join(self.flatten_data(text))
-        return text
+        text_yaml = yaml.load(text, yaml.FullLoader)
+        if text_yaml == None:
+            return []
+        text_list = self.flatten_data(text_yaml)
+        text_formatted = ", ".join(text_list)
+        return (text_formatted,)
 
+class CopyImageWayland():
+    def __init__(self):
+        pass
+    RETURN_TYPES = ()
+    CATEGORY = BASE_CATEGORY + "/image"
+    FUNCTION = "copy_img"
+    OUTPUT_NODE = True
+
+    @classmethod
+    def INPUT_TYPES(s):
+        XDG_RUNTIME_DIR=os.environ.get('XDG_RUNTIME_DIR')
+        return {"required":
+                    {"images": ("IMAGE", ), 
+                    "num": ("INT", {"default": 1, "min": 1, "max": 4096}),
+                    "XDG_RUNTIME_DIR": ("STRING", {"multiline": False, "default": XDG_RUNTIME_DIR if XDG_RUNTIME_DIR is not None else "/run/user/1000"}),
+                    },
+                }
+
+    def is_wl_copy_available(self):
+        try:
+            # 尝试运行 wl-copy 命令
+            subprocess.run(['wl-copy', '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            return True
+        except subprocess.CalledProcessError:
+            # 如果命令执行失败，则说明 wl-copy 不可用
+            return False
+
+    def copy_img(self, images, num, XDG_RUNTIME_DIR):
+
+        if not self.is_wl_copy_available():
+            print("error: wl-copy is unavailable")
+            return {}
+        elif os.environ.get('XDG_RUNTIME_DIR')==None:
+            print("warn: cannot get XDG_RUNTIME_DIR, try user input.")
+
+        if len(images) < num: 
+            num=len(images)
+        img = images[num-1]
+        i = 255. * img.cpu().numpy()
+        img = Image.fromarray(np.clip(i, 0, 255).astype(np.uint8))
+
+        with io.BytesIO() as bio:
+            # 将 PIL 图像保存到临时文件
+            img.save(bio, format='PNG')
+            # 使用 pyclip 复制文件内容到剪贴板
+            subprocess.run('wl-copy', input=bio.getvalue(), shell=True, env={"XDG_RUNTIME_DIR":XDG_RUNTIME_DIR})
+        return {}
+
+class BlackImage():
+    def __init__(self):
+        pass
+
+    RETURN_TYPES = ("IMAGE",)
+    CATEGORY = BASE_CATEGORY + "/image"
+    FUNCTION = "empty_img"
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "width": ("INT", {"default": 512, "min": 16, "max": MAX_RESOLUTION, "step": 8}),
+                              "height": ("INT", {"default": 512, "min": 16, "max": MAX_RESOLUTION, "step": 8}),
+                              "batch_size": ("INT", {"default": 1, "min": 1, "max": 4096})}}
+
+    def empty_img(self, width, height, batch_size):
+        images = [np.zeros([width,height]) for i in range(batch_size)]
+        return images
 
 NODE_CLASS_MAPPINGS = {
     "MergeStrings": MergeStrings,
     "ExecStrAsCode": ExecStrAsCode,
-    "YamlToPrompt":YamlToPrompt
+    "YamlToPrompt":YamlToPrompt,
+    "CopyImage(Wayland)":CopyImageWayland,
+    "BlackImage":BlackImage
 }
